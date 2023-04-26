38aad5cc3b241ae39dc8d8ede97c9f26a624359a
Richard Tumaneng <46977929+Hydraero@users.noreply.github.com>
2023-04-26 15:40:24 -0700

    Readme Update + Added date funcitonality

---
+++ ned-assignment/readme.md
@@ -0,0 +1,11 @@
+# Ned Assignment
+
+## Notes
+
+- funding_amount_min and funding_amount_max from the json was used as the slider minimum and maximum value for "What is your desired loan amount?" ()
+
+
+- revenue_percentage_min and revenue_percentage_max from the json was used as the minimum and maximum percentages allowed for "Repayment Rate:"
+
+
+- What will the funds be used for needs to be worked on to allow user to add Fund type, description, and amount   
--- ned-assignment/src/components/Hero.jsx
+++ ned-assignment/src/components/Hero.jsx
@@ -6,29 +6,41 @@ const Hero = () => {
     const [data, setData] = useState(null);
     const [revenue_amount, set_revenue_amount] = useState(0);
     const [funding_amount, set_funding_amount] = useState(0);
-
     const [radioInput, setRadioInput] = useState("weekly");                         //https://codesandbox.io/s/mlp6y?file=/src/App.js
+    const [repaymentDelay, setRepaymentDelay] = useState("");
+    const [fundUses, setFundUses] = useState([{
+    }]);
 
-    const[repaymentDropdownOpen, setRepaymentDropdownOpen] = useState(false);       //https://www.robinwieruch.de/react-dropdown/
-    
-    function handleRevenueChange(e){
+
+    function handleRevenueChange(e) {
         set_revenue_amount(e.target.value);
-        console.log(revenue_amount);
     }
 
-    function handleFundingChange(e){
+    function handleFundingChange(e) {
         set_funding_amount(e.target.value);
-        console.log(funding_amount);
-    } 
+    }
 
     const radioChangeHandler = (e) => {
         setRadioInput(e.target.value);
     }
 
-    const handleRepaymentDropdownOpen = () => {
-        setRepaymentDropdownOpen(!repaymentDropdownOpen);
-    };
-    
+    const repaymentDelayChangeHandler = (e) => {
+        setRepaymentDelay(e.target.value);
+    }
+
+    function handleAddFundUse() {
+        const updateFundUses = [
+            ...fundUses,
+            {
+                id: 1,
+                fundType: "Marketing",
+                description: "description",
+                amount: 0,
+            }
+        ]
+        setFundUses(updateFundUses);
+    }
+
     useEffect(() => {
         /* const getData = async () => {
             try {
@@ -78,14 +90,24 @@ const Hero = () => {
 
     //Loan Amount Items
     var loanLabel = dataStuff.get('funding_amount')?.label;
-    var loanValue = dataStuff.get('funding_amount')?.value;   
+    var loanValue = dataStuff.get('funding_amount')?.value;
     var loanValueInt = eval(loanValue);                                         //Evaluate loan value equation
 
     //Revenue Share Percentage Items
     var revenuePercentLabel = dataStuff.get('revenue_percentage')?.label;
     var revenuePercentValue = dataStuff.get('revenue_percentage')?.value;
+    var revenuePercentMin = dataStuff.get('revenue_percentage_min')?.value;
+    var revenuePercentMax = dataStuff.get('revenue_percentage_max')?.value;
     var revenuePercentValueNum = eval(revenuePercentValue);                     //Evaluate revenue share percentage equation
-    var revenuePercentValuePercent = revenuePercentValueNum*100;
+    var revenuePercentValuePercent = revenuePercentValueNum * 100;
+
+    //if revenue percentage value is less than minimum or greater than maximum, set it to min or max
+    if (revenuePercentValuePercent < revenuePercentMin) {
+        revenuePercentValuePercent = revenuePercentMin;
+    }
+    else if (revenuePercentValuePercent > revenuePercentMax) {
+        revenuePercentValuePercent = revenuePercentMax;
+    }
 
     //Revenue Share Frequency Items
     var revenueShareFreqLabel = dataStuff.get('revenue_shared_frequency')?.label;
@@ -96,35 +118,50 @@ const Hero = () => {
     //Desired Repayment Delay Items
     var repaymentDelayLabel = dataStuff.get('desired_repayment_delay')?.label;
     var repaymentDelayValue = dataStuff.get('desired_repayment_delay')?.value;
-    var repaymentDelayValueArr = repaymentDelayValue?.split("*"); 
+    var repaymentDelayValueArr = repaymentDelayValue?.split("*");
 
     //Use Of Funds Items
     var useOfFundsLabel = dataStuff.get('use_of_funds')?.label;
     var useOfFundsValue = dataStuff.get('use_of_funds')?.value;
     var useOfFundsArr = useOfFundsValue?.split("*");
 
+    //Funding Amount Min and Max (for bonus)
+    var fundingMin = dataStuff.get('funding_amount_min')?.value;
+    var fundingMax = dataStuff.get('funding_amount_max')?.value;
+
     //Results Section Items:
     //Fees
     var desiredFeesValue = Number(dataStuff.get('desired_fee_percentage')?.value);
-    var feePercentage = Number(desiredFeesValue*100);
-    var feeAmount = Number(desiredFeesValue*funding_amount);
-    var totalRevenueShare = Number(funding_amount)+feeAmount;
+
+    //Fee Percentages
+    var feePercentage = Number(desiredFeesValue * 100);
+
+    //Fee amounts
+    var feeAmount = Number(desiredFeesValue * funding_amount);
+    var totalRevenueShare = Number(funding_amount) + feeAmount;
     var expectedTransfers = 0;
-    if(radioInput == "weekly")
-    {
-        expectedTransfers = (totalRevenueShare)/(revenue_amount*revenuePercentValueNum*52);
+    if (radioInput == "weekly") {
+        expectedTransfers = (totalRevenueShare * 52) / (revenue_amount * revenuePercentValueNum);
     }
-    else if(radioInput == "monthly")
-    {
-        expectedTransfers = (totalRevenueShare)/(revenue_amount*revenuePercentValueNum*12);
+    else if (radioInput == "monthly") {
+        expectedTransfers = (totalRevenueShare * 12) / (revenue_amount * revenuePercentValueNum);
     }
+
+    const date = new Date();
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+    var repaymentDelayDaysInt = Number(repaymentDelay.replace(/\D/g, ""));
+    var expectedCompletionDate = date.setDate(date.getDate() + expectedTransfers + repaymentDelayDaysInt);
+    var d = new Date(expectedCompletionDate);
+    var formattedDate = `${monthNames[d.getMonth()]} ` + `${d.getDate()}` + "," + ` ${d.getFullYear()}`;
 
 
     return (
         <div className="flex flex-col bg-primary-grey h-screen">
 
             <div className="h-screen">
-                <div className="flex flex-row justify-center space-x-3 h-5/6">
+                <div className="flex flex-row justify-center space-x-3 h-vscreen">
                     <div className="bg-white w-3/6 mt-20 px-20 pt-3 rounded-lg shadow-lg financing-options text-sm">
                         {/*<FinancingOptions items={data} />*/}
                         <div className="pb-5">
@@ -140,43 +177,44 @@ const Hero = () => {
 
                             <div className="loan-amount">
                                 <h2>{loanLabel}</h2>
-                                <input name="rangeAmount" type="range" className="bg-primary-grey w-4/6 h-10" min="0" max={`${loanValueInt}`} onChange={handleFundingChange}></input>
-                                <input name="amount" type="number" className="bg-primary-grey w-1/6 h-10" onChange={handleFundingChange}></input>
+                                <input name="rangeAmount" type="range" className="bg-primary-grey w-4/6 h-10" min={fundingMin} max={fundingMax} onChange={handleFundingChange}></input>
+                                <input name="amount" type="number" className="bg-primary-grey w-1/6 h-10" min={fundingMin} max={fundingMax} onChange={handleFundingChange}></input>
                             </div>
 
                             <div className="repayment-rate">
-                                <h2>{revenuePercentLabel}: <span className="text-button-blue">{/*round to nearest 2 decimals*/Math.round(revenuePercentValuePercent*100)/100}%</span></h2>
+                                <h2>{revenuePercentLabel}: <span className="text-button-blue">{/*round to nearest 2 decimals*/Math.round(revenuePercentValuePercent * 100) / 100}%</span></h2>
                             </div>
 
                             <div className="repayment-frequency" >
                                 <h2 className="flex flex-row">{revenueShareFreqLabel}:
                                     <span className="flex flex-row space-x-5 pl-5">
                                         {revenueShareFreqArr?.map((revenueShareFreqItems) => (
-                                            <div key={revenueShareFreqItems}>  
-                                                <input id={revenueShareFreqArr.indexOf(revenueShareFreqItems)} 
-                                                       type="radio" 
-                                                       name="radio-buttons" 
-                                                       value={revenueShareFreqItems} 
-                                                       onChange={radioChangeHandler} 
-                                                       checked={radioInput === `${revenueShareFreqItems}`}/>
-                                                       
-                                                    <label> {revenueShareFreqItems}</label>
+                                            <div key={revenueShareFreqItems}>
+                                                <input id={revenueShareFreqArr.indexOf(revenueShareFreqItems)}
+                                                    type="radio"
+                                                    name="radio-buttons"
+                                                    value={revenueShareFreqItems}
+                                                    onChange={radioChangeHandler}
+                                                    checked={radioInput === `${revenueShareFreqItems}`} />
+
+                                                <label> {revenueShareFreqItems}</label>
                                             </div>
                                         ))}
                                     </span>
                                 </h2>
                             </div>
 
-                            <div className="repayment-delay flex flex-row space-x-5">
+                            <form className="repayment-delay flex flex-row space-x-5">
                                 <label htmlFor="repaymentDelay">Desired {repaymentDelayLabel}:</label>
-                                <select className="border repayment-delay-dropdown">
+                                <select className="border repayment-delay-dropdown" value={repaymentDelay} onChange={repaymentDelayChangeHandler}>
                                     {repaymentDelayValueArr?.map((repaymentDelayItems) => (
                                         <option key={repaymentDelayItems} value={repaymentDelayItems}>
                                             {repaymentDelayItems}
                                         </option>
                                     ))}
                                 </select>
-                            </div>
+                            </form>
+                            Date chosen: {repaymentDelay}
 
                             <div className="fund-uses">
                                 <h2 className="pb-5">{useOfFundsLabel}</h2>
@@ -188,13 +226,21 @@ const Hero = () => {
                                             </option>
                                         ))}
                                     </select>
-
                                     <input className="bg-primary-grey w-4/6 h-10 px-3 description-input" type="text" placeholder="Description"></input>
 
                                     <input className="bg-primary-grey w-2/6 h-10 px-3 amount-input" type="text" placeholder="Amount"></input>
-                                    
-                                    <button type="button"><MdAddCircleOutline className="text-xl"/></button>
+
+                                    <button type="button" onClick={handleAddFundUse}><MdAddCircleOutline className="text-xl text-button-blue add-fund-button" /></button>
                                 </form>
+                                {/* 
+                                <ul>
+                                    {fundUses?.map((fundUse) => (
+                                        <div className="flex flex-row space-x-24" key={fundUse.id}>
+                                            <p>{fundUse.fundType}</p>
+                                        </div>
+                                    ))}
+                                </ul>
+                                */}
                             </div>
                         </div>
                     </div>
@@ -227,14 +273,15 @@ const Hero = () => {
                                 <h2>Total Revenue Share</h2>
                                 <span className="ml-auto">${totalRevenueShare}</span>
                             </div>
-                            
+
                             <div className="flex flex-row">
                                 <h2>Expected transfers</h2>
                                 <span className="ml-auto">{expectedTransfers}</span>
                             </div>
-                            
-                            <div>
+
+                            <div className="flex flex-row">
                                 <h2>Expected completion date </h2>
+                                <span className="ml-auto text-button-blue">{formattedDate}</span>
                             </div>
                         </div>
                     </div>
